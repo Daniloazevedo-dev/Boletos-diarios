1cc22186d1ab1b37ce803ba9c1785a027f999aa4
Danilo Azevedo <Danilo.ann@gmail.com>
2020-05-21 13:32:19 -0300

    node e npm correção4

--- serve.js
+++ serve.js
@@ -6,8 +6,8 @@ app.use(express.static(__dirname + '/dist'));
 
 app.listen(process.env.PORT || 5000);
 
-app.get('/*'), function(req, res){
+app.get(('/*'), function(req, res){
     res.sendFile(path.join(__dirname + '/dist/index.html'));
-}
+})
 
 console.log('Console listening');
