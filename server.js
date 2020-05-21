588e6f794c7d71ca8dbf29b843e7e08d464c6fd0
Danilo Azevedo <Danilo.ann@gmail.com>
2020-05-21 14:55:26 -0300

    node e npm correção6

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static("/src" + '/build'));
+app.use(express.static("/src"));
 
 app.listen(process.env.PORT || 8080);
 
 app.get('/*', function(req, res){
-    res.sendFile(path.join("/src" + '/build/index.html'));
+    res.sendFile(path.join("/src" + '/index.html'));
 })
 
 console.log("DIRNAME: " + __dirname);
